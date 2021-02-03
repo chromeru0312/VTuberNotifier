6420eda7f8f78c13e6c6a8e274de4df9c0b66ae0
kuro1215 <s.kuro1215@gmail.com>
2021-02-03 17:53:10 +0900

    ⚡上書き保存の処理変更

--- DataManager.cs
+++ DataManager.cs
@@ -31,21 +31,25 @@ namespace VTuberNotifier
 
         public async Task<bool> DataSaveAsync<T>(string id, T obj, bool update = false, JsonSerializerOptions options = null)
         {
-            var path = GetPathFromId(id);
+            var (temp, path) = GetPathFromId(id);
             if (File.Exists(path) && !update) return false;
 
             try
             {
-                using var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
-                using var sw = new StreamWriter(fs);
+                var fs = new FileStream(temp, FileMode.Create, FileAccess.Write);
+                var sw = new StreamWriter(fs);
                 var json = JsonSerializer.Serialize(obj, options);
                 await sw.WriteAsync(json);
+                await sw.DisposeAsync();
+                await fs.DisposeAsync();
+                if (update) File.Delete(path);
+                File.Move(temp, path);
                 await LocalConsole.Log(this, new LogMessage(LogSeverity.Info, "SaveSystem", $"File Saved[ID-{id}]."));
                 return true;
             }
             catch (Exception e)
             {
-                if (File.Exists(path)) File.Delete(path);
+                if (File.Exists(temp)) File.Delete(temp);
                 await LocalConsole.Log(this, new LogMessage(LogSeverity.Error, "SaveSystem", $"File Saving is failed[ID-{id}].", e));
                 return false;
             }
@@ -59,13 +63,13 @@ namespace VTuberNotifier
             }
             else
             {
-                LocalConsole.Log(this, new LogMessage(LogSeverity.Error, "LoadSystem", $"File is not found[ID-{id}]."));
+                LocalConsole.Log(this, new LogMessage(LogSeverity.Warning, "LoadSystem", $"File is not found[ID-{id}]."));
                 return default;
             }
         }
         public bool TryDataLoad<T>(string id, out T obj, JsonSerializerOptions options = null)
         {
-            var path = GetPathFromId(id);
+            var (_, path) = GetPathFromId(id);
             if (!File.Exists(path))
             {
                 obj = default;
@@ -81,13 +85,13 @@ namespace VTuberNotifier
         public T InitDataLoad<T>(string id) where T : new()
         {
             var data = DataLoad<T>(id);
-            if (data.Equals(default(T))) data = new T();
+            if (data == null) data = new T();
             return data;
         }
 
         public bool DataRemove(string id)
         {
-            var path = GetPathFromId(id);
+            var (_, path) = GetPathFromId(id);
             if (!File.Exists(path)) return false;
             File.Delete(path);
             return true;
@@ -97,14 +101,15 @@ namespace VTuberNotifier
             return await Task.Run(() => DataRemove(id));
         }
 
-        private string GetPathFromId(string id)
+        private (string temp, string data) GetPathFromId(string id)
         {
             var split = id.Split('/');
-            string path = DataPath;
+            var path = DataPath;
             for (int i = 0; i < split.Length - 1; i++) path = Path.Combine(path, split[i]);
             if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-            path = Path.Combine(path, split[^1] + ".data");
-            return path;
+            var data = Path.Combine(path, split[^1] + ".data");
+            var temp = Path.Combine(path, split[^1] + ".tmp");
+            return (temp, data);
         }
     }
 }
