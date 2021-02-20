6162bc011edd237dafe1599bbaccbbbbb60bc16a
kuro1215 <s.kuro1215@gmail.com>
2021-02-20 17:38:41 +0900

    ➖不要なusingの削除

--- Liver/ProducedCompany.cs
+++ Liver/ProducedCompany.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace VTuberNotifier.Liver
+﻿namespace VTuberNotifier.Liver
 {
     public static class ProducedCompany
     {
