754fb669c5b3cbbbe59d98ffe7a09784faba6325
kuro1215 <s.kuro1215@gmail.com>
2021-02-20 17:43:26 +0900

    ⚡JsonConverterの追加

--- Watcher/TwitterWatcher.cs
+++ Watcher/TwitterWatcher.cs
@@ -2,8 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using VTuberNotifier;
 using VTuberNotifier.Liver;
 
 namespace VTuberNotifier.Watcher
@@ -18,15 +19,15 @@ namespace VTuberNotifier.Watcher
             var dic = new Dictionary<Address, IReadOnlyList<Tweet>>();
             foreach (var liver in LiverData.GetAllLiversList())
             {
-                if (DataManager.Instance.TryDataLoad($"twitter/{liver.TwitterId}", out List<TweetJson> list))
-                    dic.Add(liver, new List<Tweet>(list.Select(j => new Tweet(j))));
+                if (DataManager.Instance.TryDataLoad($"twitter/{liver.TwitterId}", out List<Tweet> list))
+                    dic.Add(liver, list);
                 else dic.Add(liver, new List<Tweet>());
             }
             foreach (var group in LiverGroup.GroupList)
             {
                 if (group.TwitterId == null) continue;
-                if (DataManager.Instance.TryDataLoad($"twitter/{group.TwitterId}", out List<TweetJson> list))
-                    dic.Add(group, new List<Tweet>(list.Select(j => new Tweet(j))));
+                if (DataManager.Instance.TryDataLoad($"twitter/{group.TwitterId}", out List<Tweet> list))
+                    dic.Add(group, list);
                 else dic.Add(group, new List<Tweet>());
             }
             FoundTweetList = dic;
@@ -62,6 +63,8 @@ namespace VTuberNotifier.Watcher
         }
     }
 
+    [Serializable]
+    [JsonConverter(typeof(TweetConverter))]
     public class Tweet
     {
         public long Id { get; }
@@ -72,7 +75,7 @@ namespace VTuberNotifier.Watcher
         public Tweet RetweetedTweet { get; }
         public bool IsQuoted { get; }
         public Tweet QuotedTweet { get; }
-        public TwitterURL[] URLs { get; }
+        public TwitterUrl[] Urls { get; }
 
         public Tweet(Status status)
         {
@@ -87,47 +90,123 @@ namespace VTuberNotifier.Watcher
             if (IsRetweeted) RetweetedTweet = new Tweet(status.RetweetedStatus);
             IsQuoted = status.QuotedStatus != null;
             if (IsQuoted) QuotedTweet = new Tweet(status.QuotedStatus);
-            URLs = status.Entities.Urls.Select(e => new TwitterURL(e)).ToArray();
+            Urls = status.Entities.Urls.Select(e => new TwitterUrl(e)).ToArray();
+        }
+        private Tweet(long id, long user_id, string user_name, bool reply, Tweet retweet, Tweet quote, TwitterUrl[] urls)
+        {
+            Id = id;
+            UserId = user_id;
+            UserName = user_name;
+            IsReply = reply;
+            IsRetweeted = retweet != null;
+            RetweetedTweet = retweet;
+            IsQuoted = quote != null;
+            QuotedTweet = quote;
+            Urls = urls;
         }
 
-        public Tweet(TweetJson json)
+        public class TweetConverter : JsonConverter<Tweet>
         {
-            Id = json.Id;
-            UserId = json.UserId;
-            UserName = json.UserName;
-            IsReply = json.IsReply;
-            IsRetweeted = json.IsRetweeted;
-            RetweetedTweet = json.IsRetweeted ? new(json.RetweetedTweet) : null;
-            IsQuoted = json.IsQuoted;
-            QuotedTweet = json.IsQuoted ? new(json.QuotedTweet) : null;
-            URLs = json.URLs;
+            public override Tweet Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options)
+            {
+                if (reader.TokenType != JsonTokenType.StartObject) throw new JsonException();
+
+                reader.Read();
+                reader.Read();
+                var id = reader.GetInt64();
+                reader.Read();
+                reader.Read();
+                var user = reader.GetInt64();
+                reader.Read();
+                reader.Read();
+                var name = reader.GetString();
+                reader.Read();
+                reader.Read();
+                var reply = reader.GetBoolean();
+                reader.Read();
+                reader.Read();
+                var retweet = JsonSerializer.Deserialize<Tweet>(ref reader, options);
+                reader.Read();
+                reader.Read();
+                var quote = JsonSerializer.Deserialize<Tweet>(ref reader, options);
+                reader.Read();
+                reader.Read();
+                var urls = JsonSerializer.Deserialize<TwitterUrl[]>(ref reader, options);
+
+                reader.Read();
+                if (reader.TokenType == JsonTokenType.EndObject) return new(id, user, name, reply, retweet, quote, urls);
+                throw new JsonException();
+            }
+
+            public override void Write(Utf8JsonWriter writer, Tweet value, JsonSerializerOptions options)
+            {
+                writer.WriteStartObject();
+
+                writer.WriteNumber("Id", value.Id);
+                writer.WriteNumber("UserId", value.UserId);
+                writer.WriteString("UserName", value.UserName);
+                writer.WriteBoolean("IsReply", value.IsReply);
+
+                writer.WritePropertyName("RetweetedTweet");
+                if (value.IsRetweeted) JsonSerializer.Serialize(writer, value.RetweetedTweet, options);
+                else writer.WriteNullValue();
+                writer.WritePropertyName("QuotedTweet");
+                if (value.IsQuoted) JsonSerializer.Serialize(writer, value.QuotedTweet, options);
+                else writer.WriteNullValue();
+
+                writer.WritePropertyName("Urls");
+                JsonSerializer.Serialize(writer, value.Urls, options);
+
+                writer.WriteEndObject();
+            }
         }
     }
 
     [Serializable]
-    public class TweetJson
+    [JsonConverter(typeof(TwitterUrlConverter))]
+    public struct TwitterUrl
     {
-        public long Id { get; set; }
-        public long UserId { get; set; }
-        public string UserName { get; set; }
-        public bool IsReply { get; set; }
-        public bool IsRetweeted { get; set; }
-        public TweetJson RetweetedTweet { get; set; }
-        public bool IsQuoted { get; set; }
-        public TweetJson QuotedTweet { get; set; }
-        public TwitterURL[] URLs { get; set; }
-    }
+        public string Url { get; set; }
+        public string ExpandedUrl { get; set; }
 
-    [Serializable]
-    public struct TwitterURL
-    {
-        public string URL { get; set; }
-        public string ExpandedURL { get; set; }
-
-        public TwitterURL(UrlEntity entity)
+        public TwitterUrl(UrlEntity entity)
         {
-            URL = entity.Url;
-            ExpandedURL = entity.ExpandedUrl;
+            Url = entity.Url;
+            ExpandedUrl = entity.ExpandedUrl;
+        }
+        private TwitterUrl(string url, string exp_url)
+        {
+            Url = url;
+            ExpandedUrl = exp_url;
+        }
+
+        public class TwitterUrlConverter : JsonConverter<TwitterUrl>
+        {
+            public override TwitterUrl Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options)
+            {
+                if (reader.TokenType != JsonTokenType.StartObject) throw new JsonException();
+
+                reader.Read();
+                reader.Read();
+                var url= reader.GetString();
+                reader.Read();
+                reader.Read();
+                var exurl = reader.GetString();
+
+                reader.Read();
+                if (reader.TokenType == JsonTokenType.EndObject) return new(url, exurl);
+                throw new JsonException();
+            }
+
+            public override void Write(Utf8JsonWriter writer, TwitterUrl value, JsonSerializerOptions options)
+            {
+                writer.WriteStartObject();
+
+                writer.WriteString("Url", value.Url);
+                writer.WriteString("ExpandedUrl", value.ExpandedUrl);
+
+                writer.WriteEndObject();
+            }
         }
     }
 }
