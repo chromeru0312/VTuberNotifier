d3e978c321b38a06b1f5ec91976429756d0eea45
kuro1215 <s.kuro1215@gmail.com>
2021-02-20 17:31:54 +0900

    ⚡コマンド引数の変更

--- Discord/CmdVInfo.cs
+++ Discord/CmdVInfo.cs
@@ -1,18 +1,17 @@
 ﻿using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using VTuberNotifier.Liver;
-using VTuberNotifier.Watcher;
-using VTuberNotifier.Watcher.Feed;
-using VTuberNotifier.Watcher.Store;
+using VTuberNotifier.Watcher.Event;
 
 namespace VTuberNotifier.Discord
 {
     public class CmdVInfo : CmdBase
     {
         [Command("vinfo add")]
-        public async Task AddNotify(string liver, string services)
+        public async Task AddNotify(string liver, string services, bool only = true)
         {
             if (liver == null || liver == "")
             {
@@ -31,24 +30,19 @@ namespace VTuberNotifier.Discord
                 await SendError(this, 3, "The specified river cannot be found.");
                 return;
             }
-            var sers = services.Split(',');
             var ch = new DiscordChannel(Context.Guild.Id, Context.Channel.Id);
-            foreach (var s in sers)
+            if (!DetectTypes(detail, out var types, services.Split(',')))
             {
-                Type type = DetectType(detail, s);
-                if (type == null)
-                {
-                    await SendError(this, 4, "This service is not supported/found.");
-                    return;
-                }
-                ch.SetContent(type, null);
+                await SendError(this, 4, "This service is not supported/found.");
+                return;
             }
+            foreach (var t in types) ch.SetContent(t, only, null);
             if (DiscordNotify.AddNotifyList(detail, ch)) await ReplyAsync("Success.");
             else await SendError(this, 2, "This channel is alrady added.");
         }
 
         [Command("vinfo set")]
-        public async Task SetContent(string liver, string service, string content)
+        public async Task SetContent(string liver, string service, bool only = true, string content = null)
         {
             if (liver == null || liver == "")
             {
@@ -70,13 +64,12 @@ namespace VTuberNotifier.Discord
                 return;
             }
             var ch = new DiscordChannel(Context.Guild.Id, Context.Channel.Id);
-            Type type = DetectType(detail, service);
-            if (type == null)
+            if (!DetectType(detail, out var type, service))
             {
                 await SendError(this, 4, "This service is not supported/found.");
                 return;
             }
-            ch.SetContent(type, content);
+            ch.SetContent(type, only, content);
 
             if (DiscordNotify.UpdateNotifyList(detail, ch)) await ReplyAsync("Success.");
             else await SendError(this, 2, "This channel is not alrady added.");
@@ -101,17 +94,12 @@ namespace VTuberNotifier.Discord
             var ch = new DiscordChannel(Context.Guild.Id, Context.Channel.Id);
             if (services != null && services != "")
             {
-                var sers = services.Split(',');
-                foreach (var s in sers)
+                if (!DetectTypes(detail, out var types, services.Split(',')))
                 {
-                    Type type = DetectType(detail, s);
-                    if (type == null)
-                    {
-                        await SendError(this, 4, "This service is not supported/found.");
-                        return;
-                    }
-                    ch.RemoveContent(type);
+                    await SendError(this, 4, "This service is not supported/found.");
+                    return;
                 }
+                foreach (var t in types) ch.RemoveContent(t);
                 rem = ch.MsgContentList.Count == 0;
             }
             else rem = true;
@@ -121,13 +109,14 @@ namespace VTuberNotifier.Discord
         }
 
         [Command("vinfo")]
-        public async Task RemoveNotify(params string[] args)
+        public async Task CommandHandler(params string[] args)
         {
             var list = new List<string>() { "add", "set", "remove" };
             if (list.Contains(args[0]))
             {
                 if (args.Length == 3 && args[0] == list[0]) await AddNotify(args[1], args[2]);
-                else if (args.Length == 4 && args[0] == list[1]) await SetContent(args[1], args[2], args[3]);
+                else if (args.Length == 5 && args[0] == list[1] && bool.TryParse(args[3], out var b))
+                    await SetContent(args[1], args[2], b, args[4]);
                 else if (args.Length == 2 && args[0] == list[2]) await RemoveNotify(args[1]);
                 else if (args.Length == 3 && args[0] == list[2]) await RemoveNotify(args[1], args[2]);
                 else await SendError(this, -1, "Invalid command argument.");
@@ -150,16 +139,53 @@ namespace VTuberNotifier.Discord
             else if (search[0] == "twitter") detail = LiverData.GetLiverFromTwitterId(search[1]);
             return detail;
         }
-        private static Type DetectType(LiverDetail liver, string service)
+        private static bool DetectTypes(LiverDetail liver, out Type[] types, params string[] servs)
         {
-            if (service == "youtube") return typeof(YouTubeItem);
-            else if (service == "twitter") return typeof(Tweet);
-            else if (service == "booth") return typeof(BoothProduct);
-            else if (service == "store")
+            var list = new List<Type>();
+            types = null;
+            if (servs.Length == 0) return false;
+
+            foreach(var s in servs)
             {
-                if (liver.Group == LiverGroup.Nijiasnji) return typeof(NijisanjiProduct);
+                if (s == "youtube")
+                    list.AddRange(new List<Type>()
+                    {
+                        typeof(YouTubeNewLiveEvent), typeof(YouTubeChangeInfoEvent),
+                        typeof(YouTubeDeleteLiveEvent), typeof(YouTubeStartLiveEvent)
+                    });
+                else if (s == "booth" && liver.Group.IsExistBooth)
+                    list.AddRange(new List<Type>()
+                    {
+                        typeof(BoothNewProductEvent), typeof(BoothStartSellEvent)
+                    });
+                else if (s == "store" && liver.Group.IsExistStore)
+                    list.AddRange(new List<Type>()
+                    {
+                        liver.Group.StoreInfo.NewProductEventType, liver.Group.StoreInfo.StartSaleEventType
+                    });
+                else if (DetectType(liver, out var t, s)) list.Add(t);
+                else return false;
             }
-            return null;
+            types = list.Distinct().ToArray();
+            return true;
+        }
+        private static bool DetectType(LiverDetail liver, out Type type, string serv)
+        {
+            type = null;
+            if (serv == "youtube_new") type = typeof(YouTubeNewLiveEvent);
+            else if (serv == "youtube_change") type = typeof(YouTubeChangeInfoEvent);
+            else if (serv == "youtube_delete") type = typeof(YouTubeDeleteLiveEvent);
+            else if (serv == "youtube_start") type = typeof(YouTubeStartLiveEvent);
+            else if (serv == "booth_new" && liver.Group.IsExistBooth)
+                type = typeof(BoothNewProductEvent);
+            else if (serv == "booth_start" && liver.Group.IsExistBooth)
+                type = typeof(BoothStartSellEvent);
+            else if (serv == "store_new" && liver.Group.IsExistStore)
+                type = liver.Group.StoreInfo.NewProductEventType;
+            else if (serv == "store_start")
+                type = liver.Group.StoreInfo.StartSaleEventType;
+            else return false;
+            return true;
         }
     }
 }
