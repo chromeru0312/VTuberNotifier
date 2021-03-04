ec203efbbae9ea1020654b216b5b20fd0ea0e7af
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-03-04 13:08:53 +0900

    ✨Webhook関連APIの追加

---
+++ Controllers/WebhookController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using VTuberNotifier.Liver;
+using VTuberNotifier.Notification;
+
+namespace VTuberNotifier.Controllers
+{
+    [Route("api/webhook")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<WebhookResponse> PostWebhook(WebhookRequest req)
+        {
+            if (!LiverData.DetectLiver(req.Liver, out var liver))
+                return BadRequest(new WebhookResponse(400, "The specified river cannot be found."));
+            var dest = new WebhookDestination(req.Url);
+            foreach(var content in req.Services)
+            {
+                if (!NotifyEvent.DetectType(liver, out var type, content.Service))
+                    return BadRequest(new WebhookResponse(400, "This service is not supported/found."));
+                dest.AddContent(type, content.Only, content.Content);
+            }
+
+            switch (req.ReqestType)
+            {
+                case "Add":
+                    NotifyEvent.AddWebhookList(liver, dest);
+                    break;
+                case "Update":
+                    NotifyEvent.UpdateWebhookList(liver, dest);
+                    break;
+                case "Remove":
+                    NotifyEvent.RemoveWebhookList(liver, dest);
+                    break;
+                default:
+                    return BadRequest(new WebhookResponse(400, "The specified operation cannot be ran."));
+            }
+            return new WebhookResponse(200, "OK.");
+        }
+    }
+}
---
+++ Controllers/WebhookModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace VTuberNotifier.Controllers
+{
+    public class WebhookRequest
+    {
+        public string ReqestType { get; set; }
+        public string Url { get; set; }
+        public string Liver { get; set; }
+        public List<ServiceContent> Services { get; set; }
+
+        public struct ServiceContent
+        {
+            public string Service { get; set; }
+            public bool Only { get; set; }
+            public string Content { get; set; }
+        }
+    }
+    public class WebhookResponse
+    {
+        public int Code { get; }
+        public string Message { get; }
+
+        public WebhookResponse(HttpStatusCode code, string msg) : this((int)code, msg) { }
+        public WebhookResponse(int code, string msg)
+        {
+            Code = code;
+            Message = msg;
+        }
+    }
+}
