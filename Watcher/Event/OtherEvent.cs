a33362fe9f843260dbf392c331ce0a3c5cddb883
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-03-04 13:18:14 +0900

    ⚡JsonConverterの追加

--- Watcher/Event/OtherEvent.cs
+++ Watcher/Event/OtherEvent.cs
@@ -1,20 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 using VTuberNotifier.Liver;
 using VTuberNotifier.Watcher.Feed;
 
 namespace VTuberNotifier.Watcher.Event
 {
+    [JsonConverter(typeof(PRTimesNewArticleEventConverter))]
     public class PRTimesNewArticleEvent : EventBase<PRTimesArticle>
     {
-        public PRTimesNewArticleEvent(PRTimesArticle value) : base(value, new(value.Livers)) { }
+        public PRTimesNewArticleEvent(PRTimesArticle value) : base(nameof(PRTimesNewArticleEvent), value) { }
+        protected private PRTimesNewArticleEvent(PRTimesArticle value, DateTime dt)
+             : base(nameof(PRTimesNewArticleEvent), value, dt) { }
 
         public override string GetDiscordContent(LiverDetail liver)
         {
             var format = "新しいニュースリリースが配信されました\n{Title}({Date})\n参加ライバー:{Livers: / }\n{URL}";
             return ConvertContent(format, liver);
+        }
+
+        public class PRTimesNewArticleEventConverter : EventConverter
+        {
+            private protected override EventBase<PRTimesArticle> ResultEvent(PRTimesArticle value, DateTime dt)
+                => new PRTimesNewArticleEvent(value, dt);
         }
     }
 }
