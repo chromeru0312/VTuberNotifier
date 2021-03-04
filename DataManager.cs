335ce39f2e12d80749e91fbf47ab0504b86945d4
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-03-04 13:23:03 +0900

    ⚡テキストファイル出力機能の追加

--- DataManager.cs
+++ DataManager.cs
@@ -24,22 +24,34 @@ namespace VTuberNotifier
             if (Instance == null) Instance = new DataManager();
         }
 
+        public bool StringSave(string id, string extention, string obj)
+        {
+            return StringSaveAsync(id, extention, obj).Result;
+        }
+        public async Task<bool> StringSaveAsync(string id, string extention, string obj)
+        {
+            return await DataSaveBase(id, extention, obj);
+        }
+
         public bool DataSave<T>(string id, T obj, bool update = false, JsonSerializerOptions options = null)
         {
             return DataSaveAsync(id, obj, update, options).Result;
         }
-
         public async Task<bool> DataSaveAsync<T>(string id, T obj, bool update = false, JsonSerializerOptions options = null)
         {
-            var (temp, path) = GetPathFromId(id);
+            return await DataSaveBase(id, ".data", JsonSerializer.Serialize(obj, options), update);
+        }
+
+        private async Task<bool> DataSaveBase(string id, string extention, string obj, bool update = false)
+        {
+            var (temp, path) = GetPathFromId(id, extention);
             if (File.Exists(path) && !update) return false;
 
             try
             {
                 var fs = new FileStream(temp, FileMode.Create, FileAccess.Write);
                 var sw = new StreamWriter(fs);
-                var json = JsonSerializer.Serialize(obj, options);
-                await sw.WriteAsync(json);
+                await sw.WriteAsync(obj);
                 await sw.DisposeAsync();
                 await fs.DisposeAsync();
                 if (update) File.Delete(path);
@@ -55,6 +67,22 @@ namespace VTuberNotifier
             }
         }
 
+        public string StringLoad(string id)
+        {
+            if (TryStringLoad(id, out var str))
+            {
+                return str;
+            }
+            else
+            {
+                LocalConsole.Log(this, new LogMessage(LogSeverity.Warning, "LoadSystem", $"File is not found[ID-{id}]."));
+                return null;
+            }
+        }
+        public bool TryStringLoad(string id, out string str)
+        {
+            return LoadBase(id, out str);
+        }
         public T DataLoad<T>(string id, JsonSerializerOptions options = null)
         {
             if (TryDataLoad(id, out T obj, options))
@@ -69,24 +97,31 @@ namespace VTuberNotifier
         }
         public bool TryDataLoad<T>(string id, out T obj, JsonSerializerOptions options = null)
         {
+            if (LoadBase(id, out var str))
+            {
+                obj = JsonSerializer.Deserialize<T>(str, options);
+                return true;
+            }
+            else
+            {
+                obj = default;
+                return false;
+            }
+        }
+        private bool LoadBase(string id, out string obj)
+        {
             var (_, path) = GetPathFromId(id);
             if (!File.Exists(path))
             {
-                obj = default;
+                obj = null;
                 return false;
             }
 
             using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
             using var sr = new StreamReader(fs);
-            obj = JsonSerializer.Deserialize<T>(sr.ReadToEnd(), options);
+            obj = sr.ReadToEnd();
             LocalConsole.Log(this, new LogMessage(LogSeverity.Info, "LoadSystem", $"File Loaded[ID-{id}]."));
             return true;
-        }
-        public T InitDataLoad<T>(string id) where T : new()
-        {
-            var data = DataLoad<T>(id);
-            if (data == null) data = new T();
-            return data;
         }
 
         public bool DataRemove(string id)
@@ -101,13 +136,16 @@ namespace VTuberNotifier
             return await Task.Run(() => DataRemove(id));
         }
 
-        private (string temp, string data) GetPathFromId(string id)
+        private (string temp, string data) GetPathFromId(string id, string extention = ".data")
         {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentNullException(nameof(id));
+            else if (string.IsNullOrEmpty(extention)) throw new ArgumentNullException(nameof(extention));
+            if (!extention.StartsWith('.')) extention = '.' + extention;
             var split = id.Split('/');
             var path = DataPath;
             for (int i = 0; i < split.Length - 1; i++) path = Path.Combine(path, split[i]);
             if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-            var data = Path.Combine(path, split[^1] + ".data");
+            var data = Path.Combine(path, split[^1] + extention);
             var temp = Path.Combine(path, split[^1] + ".tmp");
             return (temp, data);
         }
