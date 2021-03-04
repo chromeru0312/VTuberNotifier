bdaf38de9f583f8ce2ae5c930918b7885b448277
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-03-04 13:21:43 +0900

    ⚡取得処理の変更

--- Watcher/Store/NijisanjiWatcher.cs
+++ Watcher/Store/NijisanjiWatcher.cs
@@ -1,4 +1,5 @@
-﻿using HtmlAgilityPack;
+﻿using Discord;
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -7,6 +8,7 @@ using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using VTuberNotifier.Liver;
 
@@ -33,85 +35,93 @@ namespace VTuberNotifier.Watcher.Store
 
         public async Task<List<ProductBase>> GetNewProduct()
         {
+            await LocalConsole.Log(this, new LogMessage(LogSeverity.Debug, "NewProduct", "Start task."));
             using WebClient wc = new WebClient { Encoding = Encoding.UTF8 };
-            var htmll = await wc.DownloadStringTaskAsync("https://shop.nijisanji.jp/s/niji/item/list?so=NW");
-
             var list = new List<ProductBase>();
-            var doc = new HtmlDocument();
-            doc.LoadHtml(htmll);
+            var end = false;
+            var page = 0;
 
-            var text = "//html/body/div/main/div/div/div[@id='itemList']/div/div[@class='first-area-item']";
-            var nodes = doc.DocumentNode.SelectNodes(text);
-            for (int i = 0; i < nodes.Count; i++)
+            while (!end && page < 4)
             {
-                var n = nodes[i].SelectSingleNode("./div/div[@class='itemname']");
-                var title = n.InnerText.Trim();
-                var url = "https://shop.nijisanji.jp" + n.SelectSingleNode("./a").Attributes["href"].Value.Trim();
-                var labels = nodes[i].SelectSingleNode("./div/div[@class='tag-area']/div");
+                var htmll = await wc.DownloadStringTaskAsync($"https://shop.nijisanji.jp/s/niji/item/list?so=NW&page={page}");
+                var doc = new HtmlDocument();
+                doc.LoadHtml(htmll);
 
-                var htmlp = await wc.DownloadStringTaskAsync(url);
-                var doc1 = new HtmlDocument();
-                doc1.LoadHtml(htmlp);
-                var n1 = doc1.DocumentNode.SelectSingleNode("//html/body/div/main/div/div/div[@class='col-sm-12 col-md-6 item-main detail_right']");
-                string cate = null, genre = null;
-                var caten = n1.SelectSingleNode("./div[@class='item_category-wrap']");
-                if (caten != null) cate = caten.InnerText.Trim().Replace("カテゴリー :", "");
-                var genren = n1.SelectSingleNode("./div[@class='item_genre-wrap']");
-                if (genren != null) genre = genren.InnerText.Trim().Replace("ジャンル : ", "");
+                var text = "//html/body/div/main/div/div/div[@id='itemList']/div/div[@class='first-area-item']";
+                var nodes = doc.DocumentNode.SelectNodes(text);
+                if (nodes == null || nodes.Count == 0) break;
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    var n = nodes[i].SelectSingleNode("./div/div[@class='itemname']");
+                    var title = n.InnerText.Trim();
+                    var url = "https://shop.nijisanji.jp" + n.SelectSingleNode("./a").Attributes["href"].Value.Trim();
+                    var labels = nodes[i].SelectSingleNode("./div/div[@class='tag-area']/div");
+                    var coming = labels.SelectSingleNode("./span[@class='itemlabel itemlabel--coming']").Attributes["style"].Value.Trim() == "display: none;";
 
-                var ps = n1.SelectNodes("./div[@class='variation']/div[@class='variation-item']/div[@class='variation-wrap1']");
-                var plist = new List<(string, int)>();
-                foreach (var p in ps)
-                {
-                    var name = p.SelectSingleNode("./div[@class='variation-name']/span").InnerText.Trim();
-                    var str = p.SelectSingleNode("./div[@class='variation-price']/div").InnerText.Trim().Replace("&yen;", "");
-                    var price = int.Parse(str.Replace("\\", "").Trim(), NumberStyles.Currency, SettingData.Culture);
-                    plist.Add((name, price));
-                }
+                    var htmlp = await wc.DownloadStringTaskAsync(url);
+                    var doc1 = new HtmlDocument();
+                    doc1.LoadHtml(htmlp);
+                    var n1 = doc1.DocumentNode.SelectSingleNode("//html/body/div/main/div/div/div[@class='col-sm-12 col-md-6 item-main detail_right']");
+                    string cate = null, genre = null;
+                    var caten = n1.SelectSingleNode("./div[@class='item_category-wrap']");
+                    if (caten != null) cate = caten.InnerText.Trim().Replace("カテゴリー :", "");
+                    var genren = n1.SelectSingleNode("./div[@class='item_genre-wrap']");
+                    if (genren != null) genre = genren.InnerText.Trim().Replace("ジャンル : ", "");
 
-                DateTime? s = null, e = null;
-                var explain = doc1.DocumentNode.SelectSingleNode("//html/body/div/main/div/div/div[@id='detail-text']/div/div").InnerText;
-                /*int si, ei;
-                if (cate == "#デジタルグッズ" && genre == "#季節ボイス")
-                {
-                    if (title.Contains("コンプリートセット")) continue;
-                    string search = "【販売期間】";
-                    si = explain.IndexOf(search) + search.Length;
-                    ei = explain.IndexOf("の間、期間限定で販売！");
+                    var ps = n1.SelectNodes("./div[@class='variation']/div[@class='variation-item']/div[@class='variation-wrap1']");
+                    var plist = new List<(string, int)>();
+                    foreach (var p in ps)
+                    {
+                        var name = p.SelectSingleNode("./div[@class='variation-name']/span").InnerText.Trim();
+                        var str = p.SelectSingleNode("./div[@class='variation-price']/div").InnerText.Trim().Replace("&yen;", "");
+                        var price = int.Parse(str.Replace("\\", "").Trim(), NumberStyles.Currency, SettingData.Culture);
+                        plist.Add((name, price));
+                    }
+
+                    DateTime? s = null, e = null;
+                    var explain = doc1.DocumentNode.SelectSingleNode("//html/body/div/main/div/div/div[@id='detail-text']/div/div").InnerText.Trim();
+                    string date = null;
+                    var m1 = Regex.Match(explain, "\\d\\d?月\\d\\d?日.*\\d\\d:\\d\\d～\\d\\d?月\\d\\d?日.*\\d\\d:\\d\\d");
+                    var m2 = Regex.Match(explain, "\\d{4}年\\d\\d?月\\d\\d?日.*\\d\\d:\\d\\d～\\d{4}年\\d\\d?月\\d\\d?日.*\\d\\d:\\d\\d");
+                    if (m1.Success) date = m1.Value;
+                    else if (m2.Success) date = m2.Value;
+                    if (date != null)
+                    {
+                        var dates = date.Split('～');
+                        s = DateTime.Parse(dates[0], SettingData.Culture);
+                        e = DateTime.Parse(dates[1], SettingData.Culture);
+                    }
+
+                    var np = new NijisanjiProduct(title, url, cate, genre, explain, plist, s, e, coming);
+                    if (!FoundProducts.Contains(np)) list.Add(np);
+                    else
+                    {
+                        end = true;
+                        break;
+                    }
                 }
-                else
-                {
-                    string search = "期間】";
-                    si = explain.IndexOf(search) + search.Length;
-                    ei = si + 39;
-                }
-                if(si != -1)
-                {
-                    var date = explain[si..ei];
-                    var di = date.IndexOf('【');
-                    if (di != -1) date = date[..di];
-                    var datea = date.Split('～');
-                    s = JPDateConvert(datea[0]);
-                    e = JPDateConvert(datea[1]);
-                }*/
-                var np = new NijisanjiProduct(title, url, cate, genre, explain, plist, s, e);
-                if (!FoundProducts.Contains(np)) list.Add(np);
+                page++;
             }
+
             if (list.Count > 0)
             {
                 FoundProducts = new List<NijisanjiProduct>(FoundProducts.Concat(list.Select(p => (NijisanjiProduct)p)));
                 await DataManager.Instance.DataSaveAsync("NijisanjiStoreProduct", FoundProducts, true);
             }
+            await LocalConsole.Log(this, new LogMessage(LogSeverity.Debug, "NewProduct", "End task."));
             return list;
+        }
 
-            static DateTime JPDateConvert(string date)
-            {
-                date = date.Replace('年', '/');
-                date = date.Replace('月', '/');
-                date = date.Replace('日', ' ');
-                date = date.Remove(date.Length - 8, date.Length - 5);
-                return DateTime.Parse(date, SettingData.Culture);
-            }
+        public async Task<bool> CheckOnSale(NijisanjiProduct product)
+        {
+            using WebClient wc = new WebClient { Encoding = Encoding.UTF8 };
+            var htmll = await wc.DownloadStringTaskAsync(product.Url);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmll);
+
+            var txt = "//html/body/div/main/div/div/div[@class='col-sm-12 col-md-6 item-main detail_right']" +
+                "/div[@class='tag-area']/div/span[@class='itemlabel itemlabel--coming']";
+            return doc.DocumentNode.SelectSingleNode(txt).Attributes["style"].Value.Trim() == "display: none;";
         }
     }
 
@@ -127,13 +137,14 @@ namespace VTuberNotifier.Watcher.Store
             => new Dictionary<string, string>(base.ContentFormat) { { "Genre", Genre } };
 
         public NijisanjiProduct(string title, string url, string category, string genre, string explain,
-            List<(string, int)> items, DateTime? start = null, DateTime? end = null)
+            List<(string, int)> items, DateTime? start = null, DateTime? end = null, bool issale = false)
             : base(title, url, LiverGroup.Nijiasnji, category, items, DetectLiver(LiverGroup.Nijiasnji, explain), start, end)
         {
             Genre = genre;
+            if (start == null) IsOnSale = issale;
         }
-        protected private NijisanjiProduct(string id, string title, string url, string category, string genre,
-           List<ProductItem> items, List<LiverDetail> livers, DateTime? start, DateTime? end)
+        private NijisanjiProduct(string id, string title, string url, string category, string genre, List<ProductItem> items,
+            List<LiverDetail> livers, DateTime? start, DateTime? end)
             : base(id, title, url, LiverGroup.Nijiasnji, category, items, livers, start, end)
         {
             Genre = genre;
