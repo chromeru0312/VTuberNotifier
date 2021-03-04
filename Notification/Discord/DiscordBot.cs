9ba7c51c3c2111a889ec59c25128f9472c36c691
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-03-04 13:11:17 +0900

    🚚名前空間の変更及び不要コメントの削除

--- Notification/Discord/DiscordBot.cs
+++ Notification/Discord/DiscordBot.cs
@@ -7,7 +7,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using static VTuberNotifier.SettingData;
 
-namespace VTuberNotifier.Discord
+namespace VTuberNotifier.Notification.Discord
 {
     public class DiscordBot
     {
@@ -52,23 +52,37 @@ namespace VTuberNotifier.Discord
             {
                 await Log(new LogMessage(LogSeverity.Info, "Command", $"Command[{msg}] process is started."));
                 var context = new CommandContext(DiscordClient, msg);
-                //var dc = DiscordCommand.Instance;
-                //dc.StartCommandProcessing(context);
                 var result = await DiscordCmdService.ExecuteAsync(context, arg, ServicePrivider);
                 if (result.IsSuccess)
                 {
                     await Log(new LogMessage(LogSeverity.Info, "Command", $"Command[{msg}] is Success."));
-                    //DiscordCommand.Instance.CommandHistory[dc.ProcessingCommand[context]].SetStatus(CommandDetail.CommandStatus.Success);
                 }
                 else
                 {
                     await Log(new LogMessage(LogSeverity.Info, "Command",
                         $"Command[{msg}] is Error.\n{result.Error.Value}: {result.ErrorReason}"));
-                    //DiscordCommand.Instance.CommandHistory[dc.ProcessingCommand[context]].SetStatus(CommandDetail.CommandStatus.Error, result.ErrorReason);
                 }
             }
         }
 
         private Task Log(LogMessage msg) => LocalConsole.Log("Discord", msg);
+    }
+    public class CmdBase : ModuleBase
+    {
+        protected async Task<IUserMessage> SendError<T>(T _, int argpos, string msg) where T : CmdBase
+        {
+            var name = typeof(T).Name[3..].ToLower();
+            var s = $"{Context.User.Mention}\nIllegal Command Error: {name}\n" +
+                $"Argument No{argpos} -- {msg}";
+            await ReplyAsync(s);
+            throw new ArgumentException(msg);
+        }
+
+        protected async override Task<IUserMessage> ReplyAsync(string message = null, bool isTTS = false,
+            Embed embed = null, RequestOptions options = null, AllowedMentions allowedMentions = null, MessageReference messageReference = null)
+        {
+            var msg = await base.ReplyAsync(message, isTTS, embed, options);
+            return msg;
+        }
     }
 }
