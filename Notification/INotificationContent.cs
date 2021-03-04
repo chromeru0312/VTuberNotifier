5ab3c1bc94945adbe3cf42699a718d9699199500
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-03-04 13:15:46 +0900

    ⚡通知用クラスの廃統合

---
+++ Notification/INotificationContent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using VTuberNotifier.Liver;
+
+namespace VTuberNotifier.Notification
+{
+    public interface INotificationContent
+    {
+        public IReadOnlyList<LiverDetail> Livers { get; }
+        public IReadOnlyDictionary<string, string> ContentFormat { get; }
+        public IReadOnlyDictionary<string, IEnumerable<object>> ContentFormatEnumerator { get; }
+        public IReadOnlyDictionary<string, Func<LiverDetail, IEnumerable<string>>> ContentFormatEnumeratorFunc { get; }
+
+
+        public string ConvertDateTime(DateTime dt)
+        {
+            var now = DateTime.Now;
+            if (now.Year != dt.Year) return dt.ToString("yyyy/MM/dd HH:mm");
+            else return dt.ToString("MM/dd HH:mm");
+        }
+        public string ConvertDuringDateTime(DateTime start, DateTime? end = null)
+        {
+            if (end != null) return $"{ConvertDateTime(start)} ～ {ConvertDateTime((DateTime)end)}";
+            else return $"{ConvertDateTime(start)} ～"; ;
+        }
+    }
+}
