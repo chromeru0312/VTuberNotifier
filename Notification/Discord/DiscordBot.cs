d97a56bcde19b78874401a85b05e0ca506ed6638
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-03-24 22:38:44 +0900

    ⚡チャンネルリストの追加

--- Notification/Discord/DiscordBot.cs
+++ Notification/Discord/DiscordBot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
@@ -12,13 +13,23 @@ namespace VTuberNotifier.Notification.Discord
     public class DiscordBot
     {
         public static DiscordBot Instance { get; private set; }
-        internal IReadOnlyList<ulong> AdministratorID { get; private set; }
+        internal IReadOnlyList<(ulong, ulong)> AllChannels { get; private set; }
 
         private DiscordBot()
         {
             DiscordClient.Log += Log;
             DiscordClient.MessageReceived += CommandRecieved;
             DataManager.CreateInstance();
+
+            if (DataManager.Instance.TryDataLoad("AllDiscordList", out IEnumerable<string> list))
+                AllChannels = new List<(ulong, ulong)>(list.Select(s => GetTuple(s)));
+            else AllChannels = new List<(ulong, ulong)>();
+
+            static (ulong, ulong) GetTuple(string s)
+            {
+                var ss = s.Split('/');
+                return (ulong.Parse(ss[0].Trim()), ulong.Parse(ss[1].Trim()));
+            }
         }
 
         public static void CreateInstance()
@@ -63,6 +74,28 @@ namespace VTuberNotifier.Notification.Discord
                         $"Command[{msg}] is Error.\n{result.Error.Value}: {result.ErrorReason}"));
                 }
             }
+        }
+
+        public void AddChannel(ulong guild, ulong channel)
+        {
+            var taple = (guild, channel);
+            var list = new List<(ulong, ulong)>(AllChannels);
+            if (!list.Contains(taple)) list.Add(taple);
+            AllChannels = list;
+            SaveList();
+        }
+        public void RemoveChannel(ulong guild, ulong channel)
+        {
+            var taple = (guild, channel);
+            var list = new List<(ulong, ulong)>(AllChannels);
+            if (list.Contains(taple)) list.Remove(taple);
+            AllChannels = list;
+            SaveList();
+        }
+        private void SaveList()
+        {
+            var data = AllChannels.Select((g, c) => $"{g} / {c}");
+            DataManager.Instance.DataSave("AllDiscordList", data, true);
         }
 
         private Task Log(LogMessage msg) => LocalConsole.Log("Discord", msg);
