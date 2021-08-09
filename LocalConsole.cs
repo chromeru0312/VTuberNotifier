f8ab391f5ded6a54ca8c8378825801563e77fe6e
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-08-09 14:23:52 +0900

    🚀ログ出力方法を変更

--- LocalConsole.cs
+++ LocalConsole.cs
@@ -3,14 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace VTuberNotifier
 {
     public static class LocalConsole
     {
-        public static bool IsDebug { get; set; } = false;
+        public static string LogPath { get; set; } = null;
+        public static bool IsDebug { get; set; }
+#if DEBUG
+        = true;
+#else
+        = false;
+#endif
         private static ConsoleStream ConsoleWriter { get; set; }
-        private static ConsoleColor? DefaultColor { get; set; }
+        private static ConsoleColor DefaultColor { get; set; }
         private static IReadOnlyDictionary<LogSeverity, ConsoleColor> SeverityColor { get; }
             = new Dictionary<LogSeverity, ConsoleColor>()
             {
@@ -22,53 +29,89 @@ namespace VTuberNotifier
                 { LogSeverity.Debug, ConsoleColor.DarkGray },
             };
 
+        private static Queue<(string, LogMessage)> LogQueue { get; } = new();
+        private static Task ConsoleTask { get; set; } = Task.CompletedTask;
+
         internal static void CreateNewLogFile()
         {
-            var s = "./logs";
-            if (!Directory.Exists(s)) Directory.CreateDirectory(s);
-            var path = Path.Combine(Path.GetFullPath(s), $"log-{DateTime.Now:yyyyMMddHHmm}.log");
-            ConsoleWriter = new ConsoleStream(Console.OpenStandardOutput(), new StreamWriter(path, true, Encoding.UTF8)) { AutoFlush = true };
+            if (LogPath == null)
+            {
+                LogPath = Path.Combine(Settings.Data.ExecutingPath, "logs");
+                if (!Directory.Exists(LogPath)) Directory.CreateDirectory(LogPath);
+                DefaultColor = IsDebug ? ConsoleColor.White : Console.ForegroundColor;
+            }
+            var path = Path.Combine(LogPath, $"log-{DateTime.Now:yyyyMMddHHmm}.log");
+            ConsoleWriter = new ConsoleStream(new(path, true, Encoding.UTF8)) { AutoFlush = true };
             Console.SetOut(ConsoleWriter);
         }
 
         public static void Log<T>(T _, LogMessage msg) => Log(typeof(T).Name, msg);
         public static void Log(string place, LogMessage msg)
         {
-            LogInner(place, msg);
+            LogQueue.Enqueue((place, msg));
+            if (ConsoleTask.IsCompleted)
+            {
+                ConsoleTask = new Task(LogInner);
+                ConsoleTask.Start();
+            }
         }
-        private static void LogInner(string place, LogMessage msg)
+        private static void LogInner()
         {
-            if (DefaultColor == null) DefaultColor = Console.ForegroundColor;
-            var text = $"{DateTime.Now:HH:mm:ss.fff} {place}";
-            if (!string.IsNullOrEmpty(msg.Source) || place != msg.Source)
-                text += $"({msg.Source})";
-            var e = msg.Exception;
-            var normal = (ConsoleColor)DefaultColor;
-            var message = normal;
-
-            if (msg.Severity == LogSeverity.Critical)
-                message = ConsoleColor.Red;
-            else if (msg.Severity == LogSeverity.Debug)
+            while (LogQueue.Count > 0)
             {
-                if (IsDebug)
-                    message = ConsoleColor.DarkGray;
-                else
-                    ConsoleWriter.FileStream.WriteLine($"{text}\n             [debug] {msg.Message}");
-            }
+                var (place, msg) = LogQueue.Dequeue();
+                if (((string.IsNullOrWhiteSpace(place) && string.IsNullOrWhiteSpace(msg.Source)) ||
+                    string.IsNullOrWhiteSpace(msg.Message)) && msg.Exception == null) return;
 
-            Console.WriteLine(text);
-            Console.ForegroundColor = SeverityColor[msg.Severity];
-            Console.Write($"             [{msg.Severity.ToString().ToLower()}] ");
-            Console.ForegroundColor = message;
-            Console.WriteLine(msg.Message);
-            if (e != null && SeverityColor[msg.Severity] == ConsoleColor.Red)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine($"{e.GetType()} - {e.Message}");
-                ConsoleWriter.FileStream.WriteLine(e.StackTrace);
-            }
+                var text = $"{DateTime.Now:HH:mm:ss.fff} {place}";
+                if (!string.IsNullOrEmpty(msg.Source) && place != msg.Source)
+                    text += $"({msg.Source})";
+                var e = msg.Exception;
+                var message = DefaultColor;
 
-            Console.ForegroundColor = normal;
+                if (msg.Severity == LogSeverity.Critical)
+                    message = ConsoleColor.Red;
+                else if (msg.Severity == LogSeverity.Debug)
+                {
+                    if (IsDebug)
+                    {
+                        Console.ForegroundColor = message = ConsoleColor.DarkGray;
+                    }
+                    else
+                    {
+                        ConsoleWriter.FileStream.WriteLine($"{text}\n             [debug] {msg.Message}");
+                        continue;
+                    }
+                }
+
+                Console.WriteLine(text);
+                Console.ForegroundColor = SeverityColor[msg.Severity];
+                Console.Write($"             [{msg.Severity.ToString().ToLower()}] ");
+                Console.ForegroundColor = message;
+                Console.WriteLine(msg.Message);
+                if (e != null && SeverityColor[msg.Severity] == ConsoleColor.Red)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"{e.GetType()} - {e.Message}");
+                    if (e.StackTrace != null)
+                        ConsoleWriter.FileStream.WriteLine(e.StackTrace);
+                    var ex = e;
+                    var count = 5;
+                    while (ex.InnerException != null)
+                    {
+                        ex = e.InnerException;
+                        ConsoleWriter.FileStream.WriteLine($"--> {ex.GetType()} - {ex.Message}" +
+                            (ex.StackTrace == null ? "" : $"\n    {ex.StackTrace.Replace("\n", "\n    ")}"));
+                        count--;
+                        if (count == 0)
+                        {
+                            ConsoleWriter.FileStream.WriteLine("--> [And more inner exceptions...]");
+                            break;
+                        }
+                    }
+                }
+                Console.ForegroundColor = DefaultColor;
+            }
         }
 
         internal class ConsoleStream : StreamWriter
@@ -84,9 +127,10 @@ namespace VTuberNotifier
             }
             internal StreamWriter FileStream { get; }
 
-            public ConsoleStream(Stream console, StreamWriter file) : base(console)
+            public ConsoleStream(StreamWriter file) : base(Console.OpenStandardOutput())
             {
                 FileStream = file;
+                Console.OutputEncoding = file.Encoding;
             }
 
             public override void Write(char value)
