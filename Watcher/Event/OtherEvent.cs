f1a705798fb34cec6becceda98164d088a63a757
chromeru0312 <chromeru0312@users.noreply.github.com>
2021-09-16 21:11:10 +0900

    ✨ニコニコ生放送関連イベントの追加

--- Watcher/Event/OtherEvent.cs
+++ Watcher/Event/OtherEvent.cs
@@ -12,4 +12,22 @@ namespace VTuberNotifier.Watcher.Event
         [JsonIgnore]
         public override string FormatContent => "新しいニュースリリースが配信されました\n{Title}({Date})\n参加ライバー : {Livers: / }\n{URL}";
     }
+    public class NicoNewLiveEvent : EventBase<NicoLiveItem>
+    {
+        public override string EventTypeName => "NicoNewLive";
+        [JsonIgnore]
+        public override string FormatContent => "新しいライブ配信が作成されました\n{Title}\n参加ライバー : {Livers: / }\n{Date}\n{URL}";
+
+        public NicoNewLiveEvent(NicoLiveItem value) : this(value, DateTime.Now) { }
+        protected private NicoNewLiveEvent(NicoLiveItem value, DateTime dt) : base(value, null, dt) { }
+    }
+    public class NicoStartLiveEvent : EventBase<NicoLiveItem>
+    {
+        public override string EventTypeName => "NicoStartLive";
+        [JsonIgnore]
+        public override string FormatContent => "ライブが開始されました\n{Title}\n参加ライバー : {Livers: / }\n{Date}\n{URL}";
+
+        public NicoStartLiveEvent(NicoLiveItem value) : this(value, DateTime.Now) { }
+        protected private NicoStartLiveEvent(NicoLiveItem value, DateTime dt) : base(null, value, dt) { }
+    }
 }
